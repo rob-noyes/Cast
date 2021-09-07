d7d4fd4ff87a9655e3428b67cbf3b1625c270e7e
Robby Noyes <robertjnoyes1@gmail.com>
2021-09-06 21:13:56 -0700

    Basic functionaility for input of city, displays details for new city

--- src/modules/getContent.js
+++ src/modules/getContent.js
@@ -1,13 +1,22 @@
-const api = {
-  key: '1f82bf42f57863a5a2f579cecc0bdac0',
-  baseurl: 'https://api.openweathermap.org/data/2.5',
-};
-
 function displayWeather() {
-  async function getWeather() {
+  const api = {
+    key: '1f82bf42f57863a5a2f579cecc0bdac0',
+    baseurl: 'https://api.openweathermap.org/data/2.5',
+  };
+  let loc = document.querySelector('#getLocation');
+  let form = document.querySelector('#containerForm');
+  let input = 'Portland';
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    input = loc.value;
+    getWeather(input);
+  });
+
+  async function getWeather(input) {
     try {
       const url = await fetch(
-        `${api.baseurl}/weather?q=Portland&appid=${api.key}`
+        `${api.baseurl}/weather?q=${input}&appid=${api.key}`
       );
       const data = await url.json();
       displayData(data);
@@ -23,10 +32,6 @@ function displayWeather() {
     const date = document.querySelector('#date');
     const time = document.querySelector('#time');
     const today = new Date();
-    let hours = today.getHours();
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    let minutes = today.getUTCMinutes();
 
     location.innerHTML = data.name;
     temp.innerHTML = `${Math.round(
@@ -37,8 +42,25 @@ function displayWeather() {
       today.getMonth() + 1
     }/${today.getDate()}/${today.getFullYear()}`;
 
+    let hours = today.getHours();
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    let minutes = today.getUTCMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
     time.innerHTML = `${hours}:${minutes}`;
   }
-  getWeather();
+
+  function getInputLocation() {
+    let loc = document.querySelector('#getLocation');
+    if (loc.value === null) {
+      alert('Please enter a location');
+    } else {
+      return loc.value;
+    }
+  }
+  getWeather(input);
 }
+
 export default displayWeather;
